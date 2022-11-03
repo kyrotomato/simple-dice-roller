9f1048310795edaa5b350790c379a6e80168993e
thomas <thomasmccalister@gmail.com>
2022-11-03 14:11:18 -0500

    removed the old form and made a function for each die amount. styling

--- assets/roll.js
+++ assets/roll.js
@@ -8,40 +8,38 @@ const d10 = document.getElementById('d10');
 const d12 = document.getElementById('d12');
 const d20 = document.getElementById('d20');
 
-var diceSidesAmount = diceInput.value
 
-rollDice.onclick = function rollDice(){
-    let sides = diceInput.value;
-    var randomNumber = Math.ceil(Math.random() * sides);
-    diceResult.innerText = randomNumber;
-}
-d4.onclick = function rollDice(){
-    let sides = diceInput.value;
+d4.onclick = function rollD4(){
+    
     var randomNumber = Math.ceil(Math.random() * 4);
     diceResult.innerText = randomNumber;
+    console.log(randomNumber)
 }
-d6.onclick = function rollDice(){
-    let sides = diceInput.value;
+d6.onclick = function rollD6(){
     var randomNumber = Math.ceil(Math.random() * 6);
     diceResult.innerText = randomNumber;
+    console.log(randomNumber)
 }
-d8.onclick = function rollDice(){
-    let sides = diceInput.value;
+d8.onclick = function rollD8(){
     var randomNumber = Math.ceil(Math.random() * 8);
     diceResult.innerText = randomNumber;
+    console.log(randomNumber)
 }
-d10.onclick = function rollDice(){
-    let sides = diceInput.value;
+d10.onclick = function rollD10(){
+    
     var randomNumber = Math.ceil(Math.random() * 10);
     diceResult.innerText = randomNumber;
+    console.log(randomNumber)
 }
-d12.onclick = function rollDice(){
-    let sides = diceInput.value;
+d12.onclick = function rollD12(){
+    
     var randomNumber = Math.ceil(Math.random() * 12);
     diceResult.innerText = randomNumber;
+    console.log(randomNumber)
 }
-d20.onclick = function rollDice(){
-    let sides = diceInput.value;
+d20.onclick = function rollD20(){
+    
     var randomNumber = Math.ceil(Math.random() * 20);
     diceResult.innerText = randomNumber;
+    console.log(randomNumber)
 }
